771eb23e21a04b08a49481e6ed7968cdc1c0919e
always girish <girish_19@hotmail.com>
2024-02-06 00:56:59 +0530

    first commit

---
+++ src/main/java/com/tomato/food/tomato/TomatoFoodsApplication.java
@@ -0,0 +1,13 @@
+package com.tomato.food.tomato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomatoFoodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomatoFoodsApplication.class, args);
+	}
+
+}
